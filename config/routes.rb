232737b5384c1931ea03d7d6fe2cd6672227655b
Jack Bodley-Scott <jackso.bodleyscott@gmail.com>
2020-03-02 15:08:09 +0000

    added new match in controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  get 'matches/new'
-  get 'matches/create'
-  get 'matches/edit'
-  get 'matches/update'
-  get 'matches/destroy'
   devise_for :users
   root to: 'pages#home'
   resources :users, only: [:index, :show, :edit, :update] do
-    resources :matches, only: [:new, :create, :edit, :update]
+    resources :matches, only: [:create, :edit, :update]
   end
-  resources :matches, only: :destroy
+  resources :matches, only: [:index, :new, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
