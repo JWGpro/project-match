654249d8cf79b86a59a4b9d2a905c9e021607bad
Karl Loomes <kloomes@hotmail.com>
2020-03-02 15:30:32 +0000

    Cloudinary added, no upload links yet

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :matches
+  has_one_atached :avatar
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
