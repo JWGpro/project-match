2ed38e0d78d76257883371837054f81b11bc35a5
James Grunewald <james.grunewald.10@ucl.ac.uk>
2020-03-03 15:39:16 +0000

    added map, curated seeds

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    # @users = User.where('skill_rating...').where('availability...').near([@venue.latitude, @venue.longitude], 5)
+    @users = User.where.not(id: current_user.id)
+    @markers = @users.map do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude
+      }
+    end
+    @markers << {lat: current_user.latitude, lng: current_user.longitude}
+
   end
 
   def show
