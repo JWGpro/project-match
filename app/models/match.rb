2197803936856b3ce716ba1bd67e842bc46a8c6c
Jack Bodley-Scott <jackso.bodleyscott@gmail.com>
2020-03-03 15:14:58 +0000

    websocket messaging added

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,4 +2,5 @@ class Match < ApplicationRecord
   belongs_to :sender, class_name: 'User', foreign_key: :sender_id
   belongs_to :receiver, class_name: 'User', foreign_key: :receiver_id
   belongs_to :venue
+  has_many :messages
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
-  has_many :matches
   has_one_attached :avatar
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # has_many :matches
+  def matches
+    Match.where(sender_id: id).or(Match.where(receiver_id: id))
+  end
 end
