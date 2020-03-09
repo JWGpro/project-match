0f3e4be567ce1be5bbca55bf89ee1f6f830417ba
Pawel Maryon <maryonpawel@gmail.com>
2020-03-09 11:53:22 +0000

    message apearing on the top part of the page

--- app/javascript/channels/match_channel.js
+++ app/javascript/channels/match_channel.js
@@ -3,9 +3,14 @@ import consumer from "./consumer";
 const messagesContainer = document.querySelector('.messagess');
 if (messagesContainer) {
   const id = messagesContainer.dataset.matchId;
+  const userId = messagesContainer.dataset.userId;
   consumer.subscriptions.create({ channel: "MatchChannel", id: id }, {
     received(data) {
-      messagesContainer.insertAdjacentHTML('beforeend', data);
+      messagesContainer.insertAdjacentHTML('afterbegin', data);
+      if (userId === messagesContainer.lastElementChild.dataset.userId) {
+        messagesContainer.lastElementChild.classList.remove("message-in")
+        messagesContainer.lastElementChild.classList.add("message-out")
+      }
       console.log(data); // called when data is broadcast in the cable
     },
   });
