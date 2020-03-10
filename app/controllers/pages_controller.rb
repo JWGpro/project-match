c5488b3ddc12bbf86a5fa615a93632eec63681c9
Pawel Maryon <maryonpawel@gmail.com>
2020-03-10 12:45:11 +0000

    logout page on index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,7 @@ class PagesController < ApplicationController
     # if current_user
     #   @matches = Match.where(sender_id: current_user.id).or(Match.where(receiver_id: current_user.id))
     # end
+     
+    redirect_to new_user_session_path unless current_user.present?
   end
 end
