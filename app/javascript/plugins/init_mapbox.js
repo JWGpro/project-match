63717cf67b1fdeb0a1c0b39c683a46c40b3bb665
Karl Loomes <kloomes@hotmail.com>
2020-03-11 10:42:24 +0000

    Map dimensions and bounds padding set

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -3,7 +3,7 @@ import mapboxgl from 'mapbox-gl';
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 50, maxZoom: 17, duration: 0 });
+  map.fitBounds(bounds, { padding: 75, maxZoom: 17, duration: 0 });
 };
 
 const initMapbox = () => {
